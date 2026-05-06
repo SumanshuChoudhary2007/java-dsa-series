45d1b4b833313d71410e55e990505cc21bf72c24
sumanshu choudhary <sumanshu2007@gmail.com>
2026-05-06 15:29:34 +0530

    further in array list

--- src/Java_collection_framework/Arraylist.java
+++ src/Java_collection_framework/Arraylist.java
@@ -1,14 +1,15 @@
 package Java_collection_framework;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 public class Arraylist {
-    public static void main(String[] args){
+    static void main(String[] args) {
         //list or collection -> interfaces
 
-        ArrayList<Integer> list= new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
 //        List<Integer> list=new ArrayList<>();
 //        Collection<Integer> list=new ArrayList<>();
@@ -44,27 +45,42 @@ public class Arraylist {
 
         System.out.println(list.size()); //size of list i.e. 3
 
-        System.out.println("element in list2: "+list2);
+        System.out.println("element in list2: " + list2);
 
         list2.clear(); //remove all the element from list2
 
         System.out.println(list2); //[]
 
         // i want to iterate list using iterator
-        Iterator<Integer> iterator= list.iterator();
+        Iterator<Integer> iterator = list.iterator();
 
-        while(iterator.hasNext()){
-            System.out.println("element: "+ iterator.next());
+        while (iterator.hasNext()) {
+            System.out.println("element: " + iterator.next());
         }
 
         System.out.println(list.get(0)); //return element at particular ind
 
-        list.set(0,100); // replace the existing element at particular ind can't create new element only replace
+        list.set(0, 100); // replace the existing element at particular ind can't create new element only replace
 
         System.out.println(list.get(0));
 
-        Object[] arr=list.toArray();// converted to type array
+        Object[] arr = list.toArray();// converted to type array
 
-        System.out.println(arr.getClass());// give the 
+        System.out.println(arr.getClass());
+        System.out.println("after converted to array: " + arr);
+
+        System.out.println(list.contains(150));//false because list doesn't contain 150
+        list.add(2);
+        list.add(20);
+        list.add(15);
+        System.out.println("before sorting: " + list);
+        //  Collections.sort(list);// sort in increasing order
+        list.sort(Collections.reverseOrder());// new way to sort in java 8
+
+        System.out.println("After sorting inc order: " + list);
+        //  Collections.sort(list,Collectons.reverseOrder());
+        list.sort(Collections.reverseOrder());
+        System.out.println("After sorting desc order: " + list);
     }
+
 }
